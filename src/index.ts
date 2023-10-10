6eb7cceea5dc3498754150e5e13b60a479087d87
signe <signe.lindstrom@hotmail.com>
2023-10-10 13:37:11 +0200

    removed app folder, added logger

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import * as express from 'express';
 import helmet from 'helmet';
 import * as cors from 'cors';
 import * as compression from 'compression';
+import { log } from 'winston';
 
 const app = express();
 const hostname = process.env.NODE_HOSTNAME;
 const port = process.env.NODE_PORT;
+const logger = require('./lib/logger');
 
 app.use(helmet());
 app.options('*', cors({credentials: true, origin:true}));
@@ -18,10 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) =>{
-    res.status(200);
-    res.end('hello world');
+    // res.status(200);
+    logger.info(`[EXPRESS] path: ${req.path}, req:${req.method}, ip: ${req.ip}`);
+    res.end('hello world!');
 });
 
 app.listen(port, hostname, () => {
-    console.log(`server running att http//${hostname}:${port}`);
+    // console.log(`server running att http//${hostname}:${port}`);
+    logger.info(`[EXPRESS] Server running at http://${hostname}:${port}`)
 })
